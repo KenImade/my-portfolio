782f00aeea6c5ea8d549e99b4fc1430421026132
Kenneth Imade <kenneth.imade@yahoo.com>
2025-08-12 23:59:24 +0100

    feat: implemented recent projects section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Hero from "../components/Hero";
 import Skills from "../components/Skills";
+import RecentProjects from "../components/RecentProjects";
 
 
 const HomePage: React.FC = () => {
@@ -8,6 +9,7 @@ const HomePage: React.FC = () => {
         <div className="min-h-screen">
             <Hero/>
             <Skills/>
+            <RecentProjects/>
         </div>
     )
 }
